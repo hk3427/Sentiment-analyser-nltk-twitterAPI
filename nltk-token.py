ba390e8a5ed5331444268f11654be5a66a9f211c
HimanshuKumar111 <kumarkush36@gmail.com>
2017-01-11 03:37:11 +0530

    Adding word tokenizer

--- nltk-token.py
+++ nltk-token.py
@@ -3,3 +3,5 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 example_text = "Hello there,how are you doing? The weather is great and python is awesome."
 
 print(sent_tokenize(example_text))
+
+print(word_tokenize(example_text))
