66e3880e587bb5e2eb3d2f57f4e096b1bc44bf0b
HimanshuKumar111 <kumarkush36@gmail.com>
2017-01-10 12:09:23 +0530

    Initial commit

---
+++ nltk-token.py
@@ -0,0 +1,5 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_text = "Hello there,how are you doing? The weather is great and python is awesome."
+
+print(sent_tokenize(example_text))
